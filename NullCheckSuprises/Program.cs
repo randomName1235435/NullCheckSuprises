f884c6a9466a5b7e8deb73ceeea13fa268b94557
mm <unam3d@gmx.de>
2021-09-26 19:49:59 +0200

    init commit

---
+++ NullCheckSuprises/Program.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace NullCheckSuprises
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                BenchmarkRunner.Run<BenchmarkClass>();
+                Console.ReadLine();
+            }
+        }
+
+        [MemoryDiagnoser]
+        public class BenchmarkClass
+        {
+            object nullVariable = null;
+
+            [Benchmark]
+            public bool NullCheckIs()
+            {
+                return nullVariable is null;
+            }
+
+            [Benchmark]
+            public bool NullCheckOperator()
+            {
+                return nullVariable == null;
+            }
+
+            [Benchmark]
+            public bool NotNullCheckOperator()
+            {
+                return nullVariable != null;
+            }
+
+            [Benchmark]
+            public bool NotNullCheckIs()
+            {
+                return nullVariable is { };
+            }
+        }
+    }
